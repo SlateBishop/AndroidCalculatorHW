97bb2359e546147195f22af5f6ee314f906445d1
Despair <p.a.makulin@gmail.com>
2021-07-28 13:50:16 +0300

    init lesson4 homework

--- app/src/main/java/ru/geekbrains/makulin/mycalculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/makulin/mycalculator/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initListeners();
         calculator = new Calculator();
         setContent();
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
 
 
     }
